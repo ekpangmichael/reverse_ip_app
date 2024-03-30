f51cb9d9854d2c8dec6bdd0b4be1be825ccd88ba
Michael Ekpang <career.ekpangmichael@gmail.com>
2024-03-30 15:04:06 +0100

    remove console.log

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -3,15 +3,13 @@ const ipAddressModel = require('../models/ipAddressModel');
 exports.reverseAndStoreIP = async function(req, res) {
   const ipList = (req.headers['x-forwarded-for'] || '').split(',');
   const clientIp = ipList.length > 0 ? ipList[0].trim() : req.socket.remoteAddress.trim();
-
-  console.log("Client IP: ", clientIp);
   const reversedIp = clientIp.split('.').reverse().join('.');
 
   try {
     await ipAddressModel.insertReversedIP(reversedIp);
     res.send(`Original IP: ${clientIp} - Reversed IP: ${reversedIp}`);
   } catch (err) {
-    console.error(err); // It's a good practice to log the error for debugging purposes.
+    console.error(err); 
     res.status(500).send('Error saving the reversed IP');
   }
 };
