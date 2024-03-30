52a25ba91ef6ad2759f8c449e3d8f71fcd650989
Michael Ekpang <career.ekpangmichael@gmail.com>
2024-03-30 13:45:51 +0100

    log ip

--- controllers/ipController.js
+++ controllers/ipController.js
@@ -2,6 +2,8 @@ const ipAddressModel = require('../models/ipAddressModel');
 
 exports.reverseAndStoreIP = async function(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  console.log("THIS IS THE IP",ip);
   const reversedIp = ip.split('.').reverse().join('.');
 
   try {
